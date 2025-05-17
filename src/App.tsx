54831955473f989a2e34611cb1eecf0c37dd4545
Antonella <divirgilioantonella@gmail.com>
2025-05-17 13:59:57 -0300

    feat: :sparkles: first version working

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,80 @@
-import { useEffect } from "react"
+import { useEffect, useState, type ChangeEvent } from "react"
 import { coinbaseApi } from "./config/axios"
+import Select, { type OnChangeValue } from 'react-select'
+import makeAnimated from 'react-select/animated';
+
+type FiatCurrency = {
+  id: string;
+  name: string;
+  min_size: string;
+}
+
+type SelectOption = {
+  value: string;
+  label: string;
+}
+
+type CurrencyAPI = {
+  currency: string;
+  rates: { [key: string]: string };
+}
+
+const animatedComponents = makeAnimated();
 
 function App() {
+  const [balance, setBalance] = useState('');
+  const [rates, setRates] = useState<Record<string, string>>()
+  const [fiatCurrencies, setFiatCurrencies] = useState<SelectOption[]>([])
+  const [selectedCurrencies, setSelectedCurrencies] = useState<string[]>()
 
-  const onGetRates = async () => {
-    const response = await coinbaseApi.get("/exchange-rates?currency=USD")
+  const getFiatCurrencies = async () => {
+    const response = await coinbaseApi.get(`/currencies`)
+    const newFiatCurrencies: SelectOption[] = response?.data.data.map((currency: FiatCurrency) => ({ value: currency.id, label: currency.name }))
+    setFiatCurrencies(newFiatCurrencies)
+  }
 
-    return response
+  const getRates = async () => {
+    const response = await coinbaseApi.get("/exchange-rates?currency=EUR")
+    const data: CurrencyAPI = response?.data.data
+
+    setRates(data.rates)
+  }
+
+  const onChange = (newValue: OnChangeValue<any, true>) => {
+    const selectedValues = newValue.map(value => value.value)
+    setSelectedCurrencies(selectedValues)
+  }
+
+  const onChangeBalance = (e: ChangeEvent<HTMLInputElement>) => {
+    setBalance(e.target.value)
   }
 
   useEffect(() => {
-    onGetRates()
+    getFiatCurrencies()
+    getRates()
   }, [])
 
   return (
-    <h1 className="text-3xl font-bold">
-      Cartera Virtual de Divisas
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold">
+        Cartera Virtual de Divisas
+      </h1>
+      <input value={balance} onChange={onChangeBalance} type="number" step="0.01" required min="0.01" />
+      <Select options={fiatCurrencies} isMulti components={animatedComponents} onChange={onChange} isDisabled={!balance}/>
+
+      {
+         selectedCurrencies?.map(currency => {
+          const rate = rates?.[currency] ?? "0"
+          const exchangeValue = parseInt(balance) * parseInt(rate)
+
+          return (
+            <div key={currency} className="flex gap-5">
+              <span>{currency}</span>
+              <span>{exchangeValue}</span>
+            </div>)
+        })
+      }
+    </>
   )
 }
 
