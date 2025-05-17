17813256c2e52dd389717a5cf22556b7f8bcf1bf
Antonella <divirgilioantonella@gmail.com>
2025-05-16 21:27:32 -0300

    feat: :sparkles: create axios instance
    - create axios instance - try first connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,18 @@
+import { useEffect } from "react"
+import { coinbaseApi } from "./config/axios"
+
 function App() {
+
+  const onGetRates = async () => {
+    const response = await coinbaseApi.get("/exchange-rates?currency=USD")
+
+    return response
+  }
+
+  useEffect(() => {
+    onGetRates()
+  }, [])
+
   return (
     <h1 className="text-3xl font-bold">
       Cartera Virtual de Divisas
