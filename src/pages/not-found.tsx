23ad07e096f629b23adca0df506646924fb17c75
Antonella <divirgilioantonella@gmail.com>
2025-05-18 00:14:10 -0300

    fix: :recycle: update texts

--- src/pages/not-found.tsx
+++ src/pages/not-found.tsx
@@ -3,17 +3,17 @@ import { Link } from "react-router"
 export const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-gray-800 p-4">
-      <h1 className="text-6xl font-bold text-indigo-600 mb-4">404</h1>
-      <h2 className="text-2xl font-semibold mb-2">Página no encontrada</h2>
+      <h1 className="text-6xl font-bold text-blue-600 mb-4">404</h1>
+      <h2 className="text-2xl font-semibold mb-2">Page Not Found</h2>
       <p className="text-gray-600 mb-6 text-center max-w-md">
-        Lo sentimos, la página que estás buscando no existe.
+        We're sorry, the page you're looking for doesn't exist.
       </p>
 
       <Link
         to="/"
-        className="px-6 py-3 bg-indigo-600 text-white font-semibold rounded-md hover:bg-indigo-700 transition duration-150"
+        className="py-3 px-6 text-black font-semibold rounded-md bg-blue-300 hover:bg-blue-500 hover:text-white transition duration-150 cursor-pointer"
       >
-        Volver al Inicio
+        Return to Home
       </Link>
     </div>
   )
