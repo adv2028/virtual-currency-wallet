8e06f50face7f47747c50e26078f7f2c820df738
Antonella <divirgilioantonella@gmail.com>
2025-05-19 00:35:34 -0300

    style: :lipstick: update styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,12 @@ function App() {
   const navigation = useNavigation()
 
   return (
-    <section className="flex flex-col items-center py-5">
-      <h1 className="text-3xl font-bold">Virtual currency wallet</h1>
-      {navigation.state !== 'idle' && <>Loading...</>}
-      {navigation.state === 'idle' && <Outlet />}
+    <section className="flex flex-col items-center pt-11 bg-gray-100 h-dvh">
+      <div className='bg-white w-[300px] md:w-[500px] lg:w-[600px] min-h-40 rounded-lg shadow-xl p-6 sm:p-8'>
+        <h1 className="text-2xl font-bold text-gray-800 mb-6 text-center">Virtual currency wallet</h1>
+        {navigation.state !== 'idle' && <>Loading...</>}
+        {navigation.state === 'idle' && <Outlet />}
+      </div>
     </section>
   )
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -8,7 +8,7 @@ export const Button = ({ onClick, title, disabled }: Props) => {
   return (
     <button
       onClick={onClick}
-      className="py-3 px-6 text-black font-semibold rounded-md bg-blue-300 hover:bg-blue-500 hover:text-white transition disabled:bg-gray-200 cursor-pointer disabled:cursor-auto duration-150"
+      className="py-3 px-6 text-black font-semibold rounded-md bg-blue-300 hover:bg-blue-400 transition disabled:bg-gray-200 cursor-pointer disabled:cursor-auto duration-150 disabled:hover:text-black"
       disabled={disabled}
     >
       {title}
