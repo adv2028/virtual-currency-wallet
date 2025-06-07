1bdcca623b6fad5bc243401f0c09eacfae6baa36
adv2028 <divirgilio.antonella@gmail.com>
2025-06-07 15:42:01 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,12 @@ export default defineConfig({
     setupFiles: './src/config/setupTests.ts',
     reporters: ['default', 'html'], 
   },
+  preview: {
+		port: 3000,
+		host: true,    // This enables listening on all network interfaces
+	},
+	server: {        // Also add this for development server
+		host: true,    // This enables listening on all network interfaces
+		port: 3000
+	}
 })
